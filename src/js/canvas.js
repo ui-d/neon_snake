8ea30523efad9c7086108d892d18139db0c52ead
michu1234 <dawiddeveloper@gmail.com>
2021-01-01 23:12:16 +0100

    scaffold

---
+++ src/js/canvas.js
@@ -0,0 +1,25 @@
+import utils from "./utils";
+
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+
+// Event Listeners
+addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+});
